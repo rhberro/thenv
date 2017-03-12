c0f51512f16eef1ed48f057ba72165fd4a39651e
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-11 23:26:21 -0300

    Extract the read function to a module.

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 'use strict'
 
-var fs = require('fs')
-
 module.exports = function (filename, encoding) {
   filename = filename || '.env'
   encoding = encoding || 'utf8'
-
-  var read = function (filename, encoding) {
-    try {
-      return fs.fileReadSync(filename, encoding)
-    } catch (error) {
-      return ''
-    }
-  }
-
-  return {
-    read: read
-  }
 }
