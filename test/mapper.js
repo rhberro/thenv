bd4f113aa39a6b33af89908fff87071111d59999
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-16 22:58:50 -0300

    Add initial test files.

---
+++ test/mapper.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var assert = require('assert')
+var reader = require('../lib/reader')
+var parser = require('../lib/parser')
+var mapper = require('../lib/mapper')
+
+/* eslint-disable */
+describe( 'Mapper', function() {
+  describe( 'Map', function() {
+    it( 'should', function () {
+    } )
+  } )
+} )
+/* eslint-enable */
---
+++ test/parser.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var assert = require('assert')
+var reader = require('../lib/reader')
+var parser = require('../lib/parser')
+
+/* eslint-disable */
+describe( 'Parser', function() {
+  describe( 'Parse', function() {
+    it( 'should', function () {
+    } )
+  } )
+} )
+/* eslint-enable */
